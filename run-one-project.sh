1ac9516e2eba96a06af29593594e8aea6e7dbf1f
Owolabi Legunsen <legunsen@cornell.edu>
2023-06-11 02:00:35 -0400

    refactoring so that the script can take arguments

--- run-one-project.sh
+++ run-one-project.sh
@@ -1,4 +1,4 @@
 (
     cd scripts
-    bash run-single-project.sh 2
+    bash run-single-project.sh https://github.com/valfirst/jbehave-junit-runner jbehave-junit-runner 2 one
 )
--- scripts/run-single-project.sh
+++ scripts/run-single-project.sh
@@ -1,14 +1,23 @@
-if [ $# -lt 1 ]; then
-    echo "USAGE: bash $0 [NUM_SHAS]"
-    echo "[NUM_SHAS] is an optional argument providing the number of SHAs to run the smallest project on"
+if [ $# -lt 4 ]; then
+    echo "USAGE: bash $0 PROJECT_URL PROJECT_NAME [NUM_SHAS] [MODE:{all,one}]"
+    echo "[NUM_SHAS] is an optional argument providing the number of SHAs to run the project on"
+    echo "[MODE] is an optional argument for specifying whether to run only PS3 variant [one], or mop, tests, and [all] variants"
     exit
 fi
 
-NUM_SHAS=$1
+PROJECT_URL=$1
+PROJECT_NAME=$2
+NUM_SHAS=$3
+MODE=$4
 
 if [ "${NUM_SHAS}" == "" ]; then
-    echo "No option provided, running with 2 SHAS..."
+    echo "No [NUM_SHAS] provided, running with 2 SHAS..."
     NUM_SHAS=2
+fi
+
+if [ "${MODE}" == "" ]; then
+    echo "No [MODE] provided, running with one RPS variant (ps3)..."
+    MODE=one
 fi
 
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
@@ -16,5 +25,9 @@ SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 echo "[$0] Cleaning..."
 bash ${SCRIPT_DIR}/clean.sh
 
-echo "[$0] Running RPS-RPP-VMS on ${NUM_SHAS} SHAs..."
-bash ${SCRIPT_DIR}/run_experiment.sh https://github.com/valfirst/jbehave-junit-runner ${SCRIPT_DIR}/project_revisions/jbehave-junit-runner.txt ${NUM_SHAS} nostats rps 3 true true
+if [ "${MODE}" == "one" ]; then
+    echo "[$0] Running RPS-RPP-VMS on ${NUM_SHAS} SHAs..."
+    bash ${SCRIPT_DIR}/run_experiment.sh ${PROJECT_URL} ${SCRIPT_DIR}/project_revisions/${PROJECT_NAME}.txt ${NUM_SHAS} nostats rps 3 true true
+else
+    echo "ALL modes not yet implemented"
+fi
